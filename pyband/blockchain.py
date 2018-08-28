1556f531a1c06b6646d92629d493ad293ecddd19
bunoi <taobunoi@gmail.com>
2018-08-28 10:19:48 +0700

    equation data type parse/dump

--- pyband/blockchain.py
+++ pyband/blockchain.py
@@ -45,16 +45,16 @@ class UnsignedInteger(object):
         value = int(value)
 
         if not isinstance(value, int):
-            raise ValueError("{} must be an unsigned integer".format(value))
+            raise ValueError('{} must be an unsigned integer'.format(value))
         if value < 0 or value >= self.max_value:
-            raise ValueError("Invalid unsigned integer range {}".format(value))
+            raise ValueError('Invalid unsigned integer range {}'.format(value))
 
         return varint_encode(value)
 
     def parse(self, data):
         value = varint_decode(data)
         if value >= self.max_value:
-            raise ValueError("Invalid unsigned integer range")
+            raise ValueError('Invalid unsigned integer range')
         return self.casting(value)
 
 
@@ -68,17 +68,139 @@ class Bytes(object):
             value = bytes.fromhex(value)
 
         if len(value) != self.length:
-            raise ValueError("Invalid {} length {}".format(self.name, value))
+            raise ValueError('Invalid {} length {}'.format(self.name, value))
 
         return value
 
     def parse(self, data):
         if len(data) < self.length:
             raise ValueError(
-                "Unable to parse {} to {}. Too short".format(self.name, data))
+                'Unable to parse {} to {}. Too short'.format(self.name, data))
 
         return data[:self.length].hex()
 
+class Equation(object):
+    def __init__(self):
+        self.prcd = {'+' : 2, '-' : 2, '*' : 4, '/' : 4, '%' : 4, '^': 6, '(' : 1, ')' : 1, '#': 1 }
+
+    def tokenized(self, str):
+        token = []
+        for ch in str.replace(' ', ''):
+            if '0' <= ch <= '9':
+                if token and isinstance(token[-1], int):
+                    token.append(token.pop() * 10 + int(ch))
+                else:
+                    token.append(int(ch))
+            else:
+                token.append(ch)
+
+        return token
+
+    def infix_to_prefix(self, tokens):
+        tokens.reverse()
+        stack = []
+        new_tokens = []
+        stack.append('#')
+        for token in tokens:
+            if not(token in self.prcd):
+                new_tokens.append(token)
+            else:
+                if token == ')':
+                    stack.append('(')
+                elif token == '(':
+                    while stack[-1] != '(':
+                        new_tokens.append(stack.pop())
+                    stack.pop()
+                else:
+                    while self.prcd[stack[-1]] > self.prcd[token]:
+                        new_tokens.append(stack.pop())
+                    stack.append(token)
+
+        while stack[-1] != '#':
+            new_tokens.append(stack.pop())
+
+        return list(reversed(new_tokens))
+
+    def prefix_to_bytes(self, tokens):
+        result = b''
+        for token in tokens:
+            if token == '+':
+                result += b'\x01'
+            elif token == '-':
+                result += b'\x02'
+            elif token == '*':
+                result += b'\x03'
+            elif token == '/':
+                result += b'\x04'
+            elif token == '%':
+                result += b'\x05'
+            elif token == '^':
+                result += b'\x06'
+            elif token == 'x':
+                result += b'\x08'
+            else:
+                result += b'\x07'
+                result += varint_encode(token)
+
+        return result
+
+    def bytes_to_prefix(self, data):
+        tokens = []
+        number = bytearray()
+        in_progress = False
+        for byte in data:
+            print (byte)
+            if in_progress:
+                number += bytes([byte])
+                if not (byte & 0x80):
+                    value = varint_decode(number)
+                    number = b''
+                    tokens.append(value)
+                    in_progress = False
+            else:
+                if byte == 0x01:
+                    tokens.append('+')
+                elif byte == 0x02:
+                    tokens.append('-')
+                elif byte == 0x03:
+                    tokens.append('*')
+                elif byte == 0x04:
+                    tokens.append('/')
+                elif byte == 0x05:
+                    tokens.append('%')
+                elif byte ==     0x06:
+                    tokens.append('^')
+                elif byte == 0x07:
+                    in_progress = True
+                elif byte == 0x08:
+                    tokens.append('x')
+                else:
+                    raise ValueError('Invalid bytes string')
+        return tokens
+
+    def prefix_to_infix(self, tokens):
+        stack = []
+        for token in reversed(tokens):
+            if token in self.prcd:
+                first = stack.pop()
+                second = stack.pop()
+                stack.append('({} {} {})'.format(first, token, second))
+            else:
+                stack.append(str(token))
+
+        return stack.pop()
+
+    def dump(self, str):
+        tokens = self.tokenized(str)
+        prefix = self.infix_to_prefix(tokens)
+        data = self.prefix_to_bytes(prefix)
+
+        return data
+
+    def parse(self, data):
+        prefix = self.bytes_to_prefix(data)
+        print (prefix)
+        return self.prefix_to_infix(prefix)
 
 IDENT_LOOKUP = {
     'void': Void(),
@@ -92,7 +214,8 @@ IDENT_LOOKUP = {
     'Hash': Bytes('Hash', 32),
     'Signature': Bytes('Signature', 64),
     'Buffer': Buffer(),
-    'String': String()
+    'String': String(),
+    'Equation': Equation()
 }
 
 
@@ -108,7 +231,7 @@ class Function(object):
     def raw_tx(self, *args):
         if len(self.params) != len(args):
             raise ValueError(
-                "Invalid number of arguments to {}".format(self.name))
+                'Invalid number of arguments to {}'.format(self.name))
 
         tx_data = self.tx_prefix
         for idx in range(len(args)):
@@ -159,7 +282,7 @@ class Function(object):
         return IDENT_LOOKUP[self.result].parse(base64.b64decode(response['result']['response'].get('value', '')))
 
     def __call__(self, *args):
-        if (self.type == "action"):
+        if (self.type == 'action'):
             return self.broadcast_msg(*args)
         else:
             return self.query_msg(*args)
@@ -174,7 +297,7 @@ class Contract(object):
 
     def __getattr__(self, attr):
         if attr not in self.abi_contract:
-            raise KeyError("Invalid method {}.{}".format(self.name, attr))
+            raise KeyError('Invalid method {}.{}'.format(self.name, attr))
         return Function(
             self.config, self.name + '.' + attr, self.addr,
             **self.abi_contract[attr])
@@ -211,10 +334,10 @@ class Blockchain(object):
                 }
             })).json()
             Blockchain.abi = json.loads(base64.b64decode(
-                response['result']['response'].get('value', '')).decode("utf-8"))
+                response['result']['response'].get('value', '')).decode('utf-8'))
         self.config = config
 
     def __getattr__(self, attr):
         if attr not in self.abi:
-            raise KeyError("Invalid contract {}".format(attr))
+            raise KeyError('Invalid contract {}'.format(attr))
         return ContractCreator(self.config, attr, self.abi[attr])
